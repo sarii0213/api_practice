c672f8eaf7df48787fde919784ee6b31bfc10d86
sarii0213 <anjing.singka@gmail.com>
2023-04-30 12:57:55 +0900

    ✨ Add items controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class ItemsController < ApplicationController
+  def index
+    if params[:category_name]
+      unless Category.find_by(name: params[:category_name])
+        render json: { status: '404', message: 'requested category does not exist' }
+        return
+      end
+      items = Category.find_by(name: params[:category_name]).items.order(created_at: :desc)
+    else
+      items = Item.order(created_at: :desc)
+    end
+    render json: { status: '201', data: items }
+  end
+
+  def create
+    category = Category.find_or_create_by(name: params[:category_name])
+    item = Item.new(name: params[:name], category:)
+
+    if item.save
+      render json: { status: '201', data: item }
+    else
+      render json: { status: '422', data: item.errors }
+    end
+  end
+end
