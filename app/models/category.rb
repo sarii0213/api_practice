81f3458f1d813f61730f1f44911d80526785c1d5
sarii0213 <anjing.singka@gmail.com>
2023-04-30 12:58:27 +0900

    👕 Execute rubocop

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :items
+  has_many :items, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
